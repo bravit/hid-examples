e44c2f950f13e84f1adbab4e53e771f4cf6cab62
Vitaly Bragilevsky <bravit111@gmail.com>
2018-12-28 18:03:48 -0800

    reuse values in specs

--- tests/iplookup/LookupIPSpec.hs
+++ tests/iplookup/LookupIPSpec.hs
@@ -11,11 +11,14 @@ lookupIPSpecs = describe "LookupIPSpec" $ do
 
 spec_lookupIP :: Spec
 spec_lookupIP = do
-  let sample_iprdb = [(0,1), (100, 120)]
+  let empty_iprdb = []
+      sample_iprdb = [(0,1), (100, 120)]
+      ip1 = 110
+      ip2 = 50
   describe "lookupIP" $ do
     it "no IP in empty list" $ do
-      0 `shouldNotSatisfy` lookupIP [] 
+      ip1 `shouldNotSatisfy` lookupIP empty_iprdb
     it "IP in sample list" $ do
-      110 `shouldSatisfy` lookupIP sample_iprdb 
+      ip1 `shouldSatisfy` lookupIP sample_iprdb 
     it "no IP in sample list" $ do
-      50 `shouldNotSatisfy` lookupIP sample_iprdb 
+      ip2 `shouldNotSatisfy` lookupIP sample_iprdb 
