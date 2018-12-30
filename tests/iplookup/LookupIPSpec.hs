3fa35a967aa328231440580ec0661a01dd3b10a3
Vitaly Bragilevsky <bravit111@gmail.com>
2018-12-30 13:52:54 -0800

    tweak specs

--- tests/iplookup/LookupIPSpec.hs
+++ tests/iplookup/LookupIPSpec.hs
@@ -6,19 +6,19 @@ import Data.Word
 import LookupIP
 
 lookupIPSpecs :: Spec
-lookupIPSpecs = describe "LookupIPSpec" $ do
+lookupIPSpecs = describe "LookupIP" $ do
   spec_lookupIP
 
 spec_lookupIP :: Spec
-spec_lookupIP = do
-  let empty_iprdb = []
-      sample_iprdb = [(0,1), (100, 120)]
-      ip1 = 110
-      ip2 = 50
+spec_lookupIP =
   describe "lookupIP" $ do
-    it "no IP in empty list" $ do
+    let empty_iprdb = []
+        sample_iprdb = [(0,1), (100, 120)]
+        ip1 = 110
+        ip2 = 50
+    it "no IP in empty list" $
       ip1 `shouldNotSatisfy` lookupIP empty_iprdb
-    it "IP in sample list" $ do
+    it "IP in sample list" $
       ip1 `shouldSatisfy` lookupIP sample_iprdb 
-    it "no IP in sample list" $ do
+    it "no IP in sample list" $
       ip2 `shouldNotSatisfy` lookupIP sample_iprdb 
--- tests/iplookup/ParseIPSpec.hs
+++ tests/iplookup/ParseIPSpec.hs
@@ -7,78 +7,78 @@ import Test.Tasty.Hspec
 import ParseIP
 
 parseIPSpecs :: Spec
-parseIPSpecs = describe "ParseIPSpec" $ do
+parseIPSpecs = describe "ParseIP" $ do
   spec_buildIP
   spec_parseIP
   spec_parseIPRange
   spec_parseIPRanges
 
 spec_buildIP :: Spec
-spec_buildIP = do
+spec_buildIP =
   describe "buildIP" $ do
-    it "builds from zero" $ do
+    it "builds from zero" $
       buildIP [0,0,0,0] `shouldBe` 0
-    it "builds from one" $ do
+    it "builds from one" $
       buildIP [0,0,0,1] `shouldBe` 1
-    it "builds from localhost" $ do
+    it "builds from localhost" $
       buildIP [127,0,0,1] `shouldBe` 1 + 127 * 256^3
-    it "builds from arbitrary address" $ do
+    it "builds from arbitrary address" $
       buildIP [192,168,3,15] `shouldBe`
         15 + 3 * 256 + 168 * 256^2 + 192 * 256^3
 
 spec_parseIP :: Spec
-spec_parseIP = do
+spec_parseIP =
   describe "parseIP" $ do
-    it "parses zero" $ do
+    it "parses zero" $
       parseIP "0.0.0.0" `shouldBe` Just 0
-    it "parses one" $ do
+    it "parses one" $
       parseIP "0.0.0.1" `shouldBe` Just 1
-    it "parses the largest IP address" $ do
+    it "parses the largest IP address" $
       parseIP "255.255.255.255" `shouldBe` Just maxBound
-    it "parses some random IP address" $ do
+    it "parses some random IP address" $
       parseIP "192.168.3.15" `shouldBe`
         Just (buildIP [192,168,3,15])
-    it "fails to parse 3 components" $ do
+    it "fails to parse 3 components" $
       parseIP "192.168.1" `shouldBe` Nothing
-    it "fails to parse 4 components with postfix" $ do
+    it "fails to parse 4 components with suffix" $
       parseIP "192.168.1.1x" `shouldBe` Nothing
-    it "fails to parse empty component" $ do
+    it "fails to parse empty component" $ 
       parseIP "192.168..1" `shouldBe` Nothing
-    it "fails to parse 5 components" $ do
+    it "fails to parse 5 components" $
       parseIP "192.168.1.0.1" `shouldBe` Nothing
-    it "fails to parse too large components" $ do
+    it "fails to parse large components" $
       parseIP "256.168.1.0" `shouldBe` Nothing
-    it "fails to parse extremely large components" $ do
+    it "fails to parse extremely large components" $
       parseIP "0.0.0.4294967338" `shouldBe` Nothing
-    it "fails to parse negative components" $ do
+    it "fails to parse negative components" $
       parseIP "256.168.-1.0" `shouldBe` Nothing
-    it "fails to parse non-numeric components" $ do
+    it "fails to parse non-numeric components" $
       parseIP "192.x.1.0" `shouldBe` Nothing
 
 spec_parseIPRange :: Spec
-spec_parseIPRange = do
+spec_parseIPRange =
   describe "parseIPRange" $ do
-    it "parses pair" $ do
+    it "parses pair" $
       parseIPRange "192.168.0.1,192.168.3.100" `shouldBe`
         (,) <$> parseIP "192.168.0.1" <*> parseIP "192.168.3.100"
-    it "fails to parse single ip" $ do
+    it "fails to parse single ip" $
       parseIPRange "192.168.0.1" `shouldBe` Nothing
-    it "fails to parse single ip with comma" $ do
+    it "fails to parse single ip with comma" $
       parseIPRange "192.168.0.1," `shouldBe` Nothing
-    it "fails to parse triple" $ do
+    it "fails to parse triple" $
       parseIPRange "192.168.0.1,192.168.0.2,192.168.0.4" `shouldBe` Nothing
 
 spec_parseIPRanges :: Spec
-spec_parseIPRanges = do
-  let sample_range = "192.168.0.1,192.168.3.100"
-      sample_range2 = "0.0.0.0,0.0.0.1"
+spec_parseIPRanges =
   describe "parseIPRanges" $ do
-    it "parses empty list" $ do
+    let sample_range = "192.168.0.1,192.168.3.100"
+        sample_range2 = "0.0.0.0,0.0.0.1"
+    it "parses empty list" $
       parseIPRanges ""  `shouldBe` Right []
-    it "parses single range" $ do
+    it "parses single range" $
       parseIPRanges sample_range  `shouldBe`
         Right [fromJust $ parseIPRange sample_range]
-    it "parses two ranges" $ do
+    it "parses two ranges" $
       parseIPRanges (sample_range ++ "\n" ++ sample_range2) `shouldBe`
         Right [fromJust $ parseIPRange sample_range,
                fromJust $ parseIPRange sample_range2]
