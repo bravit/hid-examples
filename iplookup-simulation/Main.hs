352bac820ab090f56762539611cd6d6d5ec35ad3
bravit <bravit111@gmail.com>
2019-12-08 11:47:42 +0300

    final iplookup-simulation implementation

--- iplookup-simulation/Main.hs
+++ iplookup-simulation/Main.hs
@@ -3,22 +3,28 @@ module Main where
 import IPTypes
 import ParseIP
 import LookupIP
-import GenIP
-
-simulate :: IPRangeDB -> [IP] -> (Int, Int)
-simulate iprdb ips = (yes, no)
-  where
-    yes = length $ filter id $ map (lookupIP iprdb) ips
-    no = length ips - yes
+import System.Random
 
 ipdb = "data/ipdb.txt"
-nReqs = 1500000
+nReqs = 1000000
+
+simulateWith :: IPRangeDB -> Int -> (Int, Int)
+simulateWith iprdb nReqs = (yes, no)
+  where
+    ips = genIPList nReqs
+    yes = length $ filter id $ map (lookupIP iprdb) ips
+    no = nReqs - yes
+
+genIPList :: Int -> [IP]
+genIPList n = map IP $ take n $ iterate (+step) 0
+  where
+    step = fromIntegral $ 2^32 `div` n
+
+report :: (Int, Int) -> IO ()
+report info = putStrLn $ "(yes, no) = " ++ show info
 
 main = do
   iprs <- parseIPRanges <$> readFile ipdb
-  ips <- genIPList nReqs
   case iprs of
-    Right iprs -> do
-                    putStr "(yes, no) = "
-                    print $ simulate iprs ips
+    Right iprs -> report (simulateWith iprs nReqs)
     _ -> print "Can't read IP ranges database"
