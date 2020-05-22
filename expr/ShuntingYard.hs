e362ae71e1c5915c31651063a9b15e32378642b9
bravit <bravit111@gmail.com>
2020-05-22 12:14:17 +0300

    update examples

---
+++ expr/ShuntingYard.hs
@@ -0,0 +1,83 @@
+module ShuntingYard (convertToExpr) where
+
+import Data.Char (isDigit, isSpace)
+import Data.List (groupBy, intersperse)
+import Data.Foldable (traverse_)
+import Control.Monad.State
+
+import Expr
+
+-- Implementation of the Shunting-yard algorithm
+-- https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+
+type Token = String
+type Stack = [Token]
+type Output = [Expr Integer]
+type SYState = (Stack, Output)
+
+isEmpty :: State SYState Bool
+isEmpty = null <$> gets fst
+
+notEmpty :: State SYState Bool
+notEmpty = not <$> isEmpty
+
+top :: State SYState Token
+top = gets (head . fst) -- let it crash on empty stack
+
+pop :: State SYState Token
+pop = do
+  (s, es) <- get
+  put (tail s, es) -- let it crash on empty stack
+  pure (head s)
+
+pop_ :: State SYState ()  -- let it crash on empty stack
+pop_ = modify (\(s, es) -> (tail s, es))
+
+push :: Token -> State SYState ()
+push t = modify (\(s, es) -> (t : s, es))
+
+whileNotEmptyAnd :: (Token -> Bool) -> State SYState () -> State SYState ()
+whileNotEmptyAnd pred m = go
+  where
+    go = do
+      b1 <- notEmpty
+      when b1 $ do
+        b2 <- pred <$> top
+        when b2 (m >> go)
+
+output :: Token -> State SYState ()
+output t = modify (builder t <$>)
+  where
+    builder "+" (e1 : e2 : es) = Add e1 e2 : es
+    builder "*" (e1 : e2 : es) = Mult e1 e2 : es
+    builder n es = Lit (read n) : es -- let it crash on not a number
+
+isOp "+" = True
+isOp "*" = True
+isOp _ = False
+
+precedence "+" = 1
+precedence "*" = 2
+precedence _ = 0
+
+t1 `precGTE` t2 = precedence t1 >= precedence t2
+
+convertToExpr :: String -> Expr Integer
+convertToExpr str = head $ snd $ execState shuntingYard ([], [])
+  where
+    tokens = reverse $ tokenize str
+
+    shuntingYard = traverse_ processToken tokens >> transferRest
+
+    processToken ")" = push ")"
+    processToken "(" = transferWhile (/= ")") >> pop_
+    processToken t
+      | isOp t = transferWhile (`precGTE` t) >> push t
+      | otherwise = output t -- number
+
+    transfer = pop >>= output
+    transferWhile pred = whileNotEmptyAnd pred transfer
+    transferRest = transferWhile (const True)
+
+    tokenize = groupBy (\a b -> isDigit a && isDigit b)
+               . filter (not . isSpace)
---
+++ expr/prefix-postfix.hs
@@ -0,0 +1,29 @@
+import Data.List
+
+import Expr
+import ShuntingYard
+
+-- Converting expressions to prefix and postfix forms
+
+data ExprForm = Prefix | Postfix
+
+exprTo _ (Lit a) = show a
+exprTo form (Add e1 e2) = binOp "+" form e1 e2
+exprTo form (Mult e1 e2) = binOp "*" form e1 e2
+
+binOp op form e1 e2 = concat $ intersperse " " (args form)
+   where
+     e1' = exprTo form e1
+     e2' = exprTo form e2
+     args Prefix = [op, e1', e2']
+     args Postfix = [e1', e2', op]
+
+main = mapM_ printExpr strs
+  where
+    strs = ["42", "12 + 13", "(2+3*3)*5", "1+(1+2)*(2+2*(1+2))+1+3*2"]
+    printExpr str = do
+      putStrLn $ "Expression: " ++ str
+      let expr = convertToExpr str
+      putStrLn $ "Prefix form: " ++ exprTo Prefix expr
+      putStrLn $ "Postfix form: " ++ exprTo Postfix expr
+      putStrLn "-------------"
