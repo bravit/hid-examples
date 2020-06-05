9046bcdbdddf7740a6baae8be79ad45c0fb5b1e4
bravit <bravit111@gmail.com>
2020-06-05 15:11:05 +0300

    add example

---
+++ ch07/div.hs
@@ -0,0 +1,56 @@
+import Control.Exception (throw, throwIO)
+import Control.Monad.Catch
+
+
+data MyArithException = DivByZero | OtherArithException
+ deriving (Show)
+
+instance Exception MyArithException
+
+divPure :: Int -> Int -> Int
+divPure _ 0 = throw DivByZero
+divPure a b = a `div` b
+
+mult :: Int -> Int -> Int
+mult 0 _ = 0
+mult a b = a * b
+
+divIO :: Int -> Int -> IO Int
+divIO _ 0 = throwIO DivByZero
+divIO a b = pure (a `div` b)
+
+divM :: MonadThrow m => Int -> Int -> m Int
+divM _ 0 = throwM DivByZero
+divM a b = pure (a `div` b)
+
+testComputation :: MonadThrow m => Int -> Int -> Int -> m Int
+testComputation a b c = divM a b >>= divM c
+
+divTestWithRecovery ::  Int -> Int -> Int -> IO Int
+divTestWithRecovery a b c =
+    try (testComputation a b c)
+    >>= pure . dealWith
+  where
+    dealWith :: Either MyArithException Int -> Int
+    dealWith (Right r) = r
+    dealWith (Left _) = 0
+
+divTestWithRecovery2 ::  Int -> Int -> Int -> IO Int
+divTestWithRecovery2 a b c =
+    tryJust isDivByZero (testComputation a b c) >>= pure . dealWith
+  where
+    isDivByZero :: MyArithException -> Maybe ()
+    isDivByZero DivByZero = Just ()
+    isDivByZero _ = Nothing
+    dealWith (Right r) = r
+    dealWith (Left _) = 0
+
+
+divTestIO :: Int -> Int -> Int -> IO Int
+divTestIO a b c = testComputation a b c `catch` handler
+  where
+    handler :: MyArithException -> IO Int
+    handler e = do
+      putStrLn $ "We've got an exception: " ++ show e
+                 ++ "\nUsing default value 0"
+      pure 0
