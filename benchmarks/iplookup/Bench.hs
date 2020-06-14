fa3205a498b3dd4858a18970657db1ffc5f6f642
bravit <bravit111@gmail.com>
2020-06-14 12:15:42 +0300

    tweak iplookup benchmarks

--- benchmarks/iplookup/Bench.hs
+++ benchmarks/iplookup/Bench.hs
@@ -1,15 +1,17 @@
 import Criterion.Main
 
---- import BenchBuildIP
+--import BenchBuildIP
 import BenchBuildIPGroups
-import BenchParseIP
-import BenchRanges
-import BenchLookupIP
+--import BenchParseIP
+--import BenchRanges
+--import BenchLookupIP
+
 
 main :: IO ()
 main = defaultMain benchmarks
   where
-    benchmarks = bench_buildIP
-                 <> bench_parseIP
-                 <> bench_ranges
-                 <> bench_lookupIP
+    benchmarks = bench_buildIP -- <> bench_buildIP_list
+--                 <> bench_parseIP
+--                 <> bench_ranges
+--                 <> bench_lookupIP
+
--- benchmarks/iplookup/BenchBuildIP.hs
+++ benchmarks/iplookup/BenchBuildIP.hs
@@ -1,17 +1,29 @@
 module BenchBuildIP where
 
 import Criterion.Main
-import Data.Word
-
-import NFUtils ()
 import ParseIP
 
-theip :: [Word8]
-theip = [17,0,32,2]
+import NFUtils ()
 
 bench_buildIP :: [Benchmark]
 bench_buildIP = [
-    bench "buildIP-foldr" $ nf buildIP_foldr theip
-  , bench "buildIP-foldl" $ nf buildIP_foldl theip
-  , bench "buildIP-foldl-shl" $ nf buildIP_foldl_shl theip
+    bench "buildIP-foldr" $ whnf buildIP_foldr theip
+  , bench "buildIP-foldl" $ whnf buildIP_foldl theip
+  , bench "buildIP-foldl-shl" $ whnf buildIP_foldl_shl theip
   ]
+  where
+    theip = [17,0,32,2]
+
+bench_buildIP_list :: [Benchmark]
+bench_buildIP_list = [
+    bench "buildIP-foldr" $ nf (map buildIP_foldr) ipcomps
+  , bench "buildIP-foldl" $ nf (map buildIP_foldl) ipcomps
+  , bench "buildIP-foldl-shl" $ nf (map buildIP_foldl_shl) ipcomps
+  ]
+  where
+    ipcomps = [ [0,0,0,1]
+          , [192,168,1,1]
+          , [255,255,252,41]
+          , [255,255,252,41]
+          , [17,0,32,2]
+          ]
