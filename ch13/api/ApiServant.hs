0f2a0c553dcfa60d792607bcc7a1b9a0059633c5
bravit <bravit111@gmail.com>
2020-07-10 08:46:57 +0300

    add small servant example

---
+++ ch13/api/ApiServant.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import GHC.Generics
+import Data.Aeson
+
+import Servant
+import Servant.HTML.Blaze
+import qualified Text.Blaze.Html5 as H
+import Network.Wai.Handler.Warp
+
+data Rating = Bad | Good | Great
+  deriving (Show, Generic, ToJSON)
+
+data ServiceStatus = Ok | Down
+  deriving (Show, Generic, ToJSON)
+
+type BookID = Int
+
+type BookInfoAPI = Get '[JSON] ServiceStatus
+                   :<|> "title" :> Capture "id" BookID :> Get '[HTML] H.Html
+                   :<|> "year" :> Capture "id" BookID :> Get '[JSON] Int
+                   :<|> "rating" :> Capture "id" BookID :> Get '[JSON] Rating
+
+impl :: Server BookInfoAPI
+impl = pure Ok
+       :<|> title
+       :<|> year
+       :<|> rating
+  where
+    title _ = pure $ H.toHtml $ H.b "Haskell in Depth"
+    year _ = pure 2020
+    rating _ = pure Great
+
+bookInfoAPI :: Proxy BookInfoAPI
+bookInfoAPI = Proxy
+
+app :: Application
+app = serve bookInfoAPI impl
+
+main :: IO ()
+main = run 8081 app
