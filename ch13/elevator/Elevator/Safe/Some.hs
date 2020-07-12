88064edfcc09dcf8d34dfb12798c618058fd92ea
bravit <bravit111@gmail.com>
2020-07-12 07:34:23 +0300

    simplify Elevator constructors

--- ch13/elevator/Elevator/Safe/Some.hs
+++ ch13/elevator/Elevator/Safe/Some.hs
@@ -3,13 +3,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
 
-
 module Elevator.Safe.Some where
 
 import Numeric.Natural
 import Data.Type.Nat
 import Data.Proxy
-import Data.Singletons
 
 import Elevator.Safe.Floor
 import Elevator.Safe.Primitive
@@ -18,14 +16,14 @@ data SomeFloor (mx :: Nat) where
   MkSomeFloor :: Floor mx cur -> SomeFloor mx
 
 data SomeElevator (mx :: Nat) where
-  MkSomeElevator :: SingI door => Elevator mx cur door -> SomeElevator mx
+  MkSomeElevator :: Elevator mx cur door -> SomeElevator mx
 
 instance Show (SomeElevator mx) where
   show (MkSomeElevator el) = show el
 
 mkSomeFloor :: forall mx. SNatI mx => Natural -> Maybe (SomeFloor mx)
-mkSomeFloor cur = reify (fromNatural cur) toSomeFloor
+mkSomeFloor cur = reify (fromNatural cur) (fmap MkSomeFloor . toMbFloor)
   where
-    toSomeFloor :: forall cur. SNatI cur => Proxy cur -> Maybe (SomeFloor mx)
-    toSomeFloor _ = MkSomeFloor <$> (mkFloor :: Maybe (Floor mx cur))
+    toMbFloor :: SNatI cur => Proxy cur -> Maybe (Floor mx cur)
+    toMbFloor _ = mkFloor
 
