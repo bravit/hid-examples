37dc1e00fd97da2d5b1231f6ab0939ae46735e58
bravit <bravit111@gmail.com>
2020-08-12 11:13:34 +0300

    add example to ch14

---
+++ ch14/csv-simple.hs
@@ -0,0 +1,41 @@
+import Data.ByteString (ByteString)
+--import qualified Data.ByteString as B
+import Data.Text (Text)
+import qualified Data.Text.Encoding as T
+import Data.Attoparsec.ByteString.Char8 as A
+import Control.Applicative
+
+
+import qualified Streaming.Prelude as S
+import qualified Data.ByteString.Streaming as BS
+import Control.Monad.Trans.Resource
+import Data.Attoparsec.ByteString.Streaming
+import Data.Function ((&))
+import Data.Functor (void)
+
+
+field :: Parser ByteString
+field = A.takeWhile (\c -> c /= ',' && c /= '\r' && c /= '\n')
+
+textField :: Parser Text
+textField = T.decodeUtf8 <$> field
+
+record :: Parser [Text]
+record = textField `sepBy1` (char ',')
+
+endOfFile :: Parser ()
+endOfFile = endOfInput <|> endOfLine *> endOfInput
+
+file :: Parser [[Text]]
+file =
+  (:) <$> record
+      <*> manyTill (endOfLine *> record)
+                   endOfFile
+
+main :: IO ()
+-- main = B.readFile "data/quotes.csv" >>= print . parseOnly file
+main = BS.readFile "data/quotes.csv"
+       & parsed file
+       & void
+       & S.print
+       & runResourceT
